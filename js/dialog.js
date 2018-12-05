c1485bf2dbb5dc2fa16563e4681f2b1aacda1305
Marina Sidorkina <m.sidorkina55@gmail.com>
2018-12-05 15:35:26 +0300

    Добавляет выполнение задания к пятой лекции: разделяй и властвуй

--- js/dialog.js
+++ js/dialog.js
@@ -1,58 +1,54 @@
 'use strict';
 
-var setupBlock = document.querySelector('.setup');
-var setupHandler = document.querySelector('.setup-user-pic');
+(function () {
+  var setupHandler = document.querySelector('.setup-user-pic');
 
-var checkChangeValue = function (change) {
-  if (change) {
-    var onClickPreventDefault = function (evt) {
-      evt.preventDefault();
-      setupHandler.removeEventListener('click', onClickPreventDefault);
+  var checkChangeValue = function (change) {
+    if (change) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        setupHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      setupHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  var getSetupHandlerCoordinates = function (evt) {
+    return {
+      x: evt.clientX,
+      y: evt.clientY
     };
-    setupHandler.addEventListener('click', onClickPreventDefault);
-  }
-};
-
-var setSetupBlockCoordinates = function (shift) {
-  setupBlock.style.top = (setupBlock.offsetTop - shift.y) + 'px';
-  setupBlock.style.left = (setupBlock.offsetLeft - shift.x) + 'px';
-};
-
-var getSetupHandlerCoordinates = function (evt) {
-  return {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-};
-
-var getShiftCoordinates = function (startCoordinates, moveEvt) {
-  return {
-    x: startCoordinates.x - moveEvt.clientX,
-    y: startCoordinates.y - moveEvt.clientY
-  };
-};
-
-setupHandler.style.zIndex = 1;
-setupHandler.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-  var startCoordinates = getSetupHandlerCoordinates(evt);
-  var change = false;
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    change = true;
-    var shift = getShiftCoordinates(startCoordinates, moveEvt);
-    startCoordinates = getSetupHandlerCoordinates(moveEvt);
-    setSetupBlockCoordinates(shift);
   };
 
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-    checkChangeValue(change);
+  var getShiftCoordinates = function (startCoordinates, moveEvt) {
+    return {
+      x: startCoordinates.x - moveEvt.clientX,
+      y: startCoordinates.y - moveEvt.clientY
+    };
   };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+  setupHandler.style.zIndex = 1;
+  setupHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoordinates = getSetupHandlerCoordinates(evt);
+    var change = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      change = true;
+      var shift = getShiftCoordinates(startCoordinates, moveEvt);
+      startCoordinates = getSetupHandlerCoordinates(moveEvt);
+      window.setSetupBlockCoordinates(shift);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      checkChangeValue(change);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
